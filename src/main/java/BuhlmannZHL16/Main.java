55a832f725b903228bef2be2c1e0cac59dd436fc
Jens-Horstmann <Jens_Horstmann@gmx.net>
2016-01-17 05:01:35 +0800

    Gradient Factors... gfHigh working now

--- src/main/java/BuhlmannZHL16/Main.java
+++ src/main/java/BuhlmannZHL16/Main.java
@@ -9,12 +9,12 @@ public class Main {
     public static void main(String[] args) {
         ZHL16 zhl16 = new ZHL16();
 
-        zhl16.dive(4.6, 10);
-        zhl16.dive(4.6, 240);
-        zhl16.dive(1, 10);
+//        zhl16.dive(4.6, 10);
+//        zhl16.dive(4.6, 240);
+//        zhl16.dive(1, 10);
 
         for(int i=0; i<16; i++){
-            System.out.println(zhl16.getTissueLoad(i));
+            System.out.println(zhl16.getTissueLoad(i) + ", " + zhl16.getToleratedTissueLoad(i,1) + ", " + zhl16.getAscentCeiling(4.6) + ", " + zhl16.getNdl(4.6)/60);
         }
 
 
